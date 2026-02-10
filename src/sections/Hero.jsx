d7f2028e42e0014297b5ebbdd3650bda4b7d8f53
jamila ait <hamouni5099@gmail.com>
2026-02-10 16:32:59 +0100

    first section

---
+++ src/sections/Hero.jsx
@@ -0,0 +1,10 @@
+function Hero(){
+  return (
+    <section>
+      <h1>Jamila Ait Hamou</h1>
+      <h2>Front-End Developer</h2>
+      <p>I build responsive web applications using React and JavaScript.</p>
+    </section>
+  )
+}
+export default Hero;
