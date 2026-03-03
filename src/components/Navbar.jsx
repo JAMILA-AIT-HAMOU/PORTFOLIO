d7fc89f74c3e19bec081c095b11e5ccef5dda5f5
jamila ait <hamouni5099@gmail.com>
2026-03-03 21:30:27 +0000

    fixed navbar and some stilling

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -5,8 +5,8 @@ function Navbar() {
     <nav>
       <a href="#"><img src={logo} alt="Jamila's logo" className="logo"/></a>
       <ul>
-        <li onClick={()=>document.getElementById("projects").scrollIntoView({behavior: "smooth"})} >Projects</li>
         <li onClick={()=>document.getElementById("skills").scrollIntoView({behavior: "smooth"})} >Skills</li>
+        <li onClick={()=>document.getElementById("projects").scrollIntoView({behavior: "smooth"})} >Projects</li>
         <li onClick={()=>document.getElementById("certifications").scrollIntoView({behavior: "smooth"})} >Certifications</li>
         <li onClick={()=>document.getElementById("contact").scrollIntoView({behavior: "smooth"})} >Contact</li>
       </ul>
