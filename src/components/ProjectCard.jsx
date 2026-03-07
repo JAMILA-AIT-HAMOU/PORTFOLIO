17f3909640ad1f7b9e37cbfd6ec4c6a951d29132
jamila ait <hamouni5099@gmail.com>
2026-03-07 09:36:16 +0000

    Added more projects and thier stack

--- src/components/ProjectCard.jsx
+++ src/components/ProjectCard.jsx
@@ -1,14 +1,36 @@
-function ProjectCard({title,descreption, github, demo, imgSrc}){
-  return(
-    <div  className="card">
+function ProjectCard({
+  title,
+  descreption,
+  github,
+  demo,
+  imgSrc,
+  icon1,
+  icon2,
+  icon3,
+  icon4,
+}) {
+  return (
+    <div className="card">
       <img src={imgSrc} alt={title} />
       <h3>{title}</h3>
       <p>{descreption}</p>
-      <div className="link">
-        <a href={github} target="_blank">GitHub</a>
-        <a href={demo} target="_blank">Live Demo</a>
+      <div className="card-items">
+        <div className="link">
+          <a href={github} target="_blank">
+            GitHub
+          </a>
+          <a href={demo} target="_blank">
+            Live Demo
+          </a>
+        </div>
+        <div className="icons">
+          {icon1 && <img src={icon1} alt="HTML" className="icon" />}
+          {icon2 && <img src={icon2} alt="CSS" className="icon" />}
+          {icon3 && <img src={icon3} alt="JavaScript" className="icon" />}
+          {icon4 && <img src={icon4} alt="React" className="icon" />}
+        </div>
       </div>
     </div>
-  )
+  );
 }
 export default ProjectCard;
--- src/sections/Projects.jsx
+++ src/sections/Projects.jsx
@@ -2,6 +2,13 @@ import ProjectCard from "../components/ProjectCard";
 import screenShot1 from "../assets/projectsScreen/memory-card-game.png"
 import screenShot2 from "../assets/projectsScreen/currency-converter.png"
 import screenShot3 from "../assets/projectsScreen/event-rsvp-form.png"
+import screenShot4 from "../assets/projectsScreen/tic-tac-toe.png"
+import screenShot5 from "../assets/projectsScreen/dental-website.png"
+import screenShot6 from "../assets/projectsScreen/ecobrew.png"
+import HTMLIcon from "../assets/stack/HTML.png"
+import CSSIcon from "../assets/stack/CSS.png"
+import JavaScriptIcon from "../assets/stack/JavaScript.png"
+import ReactIcon from "../assets/stack/React.png"
 
 function Projects(){
   return(
@@ -15,21 +22,66 @@ function Projects(){
             title="Memory Card Game"
             descreption="React memory game with timer, sound effects, and win/lose logic."
             github="https://github.com/JAMILA-AIT-HAMOU/Memory-Card-Game" 
-            demo="https://jamila-ait-hamou.github.io/Memory-Card-Game/"/>
+            demo="https://jamila-ait-hamou.github.io/Memory-Card-Game/"
+            icon1={HTMLIcon}
+            icon2={CSSIcon}
+            icon3={JavaScriptIcon}
+            icon4={ReactIcon}/>
+
+      <ProjectCard
+            imgSrc={screenShot5}
+            title="Dental Prosthetist Website"
+            descreption="A fully responsive portfolio website built from scratch using HTML and CSS." 
+            github="https://github.com/JAMILA-AIT-HAMOU/PORTFOLIO-PROTHESISTE-DENTAIRE" 
+            demo="https://jamila-ait-hamou.github.io/PORTFOLIO-PROTHESISTE-DENTAIRE/#"
+            icon1={HTMLIcon}
+            icon2={CSSIcon}
+            icon3={JavaScriptIcon}/>
+            
+      
 
       <ProjectCard
             imgSrc={screenShot2}
             title="Currency Converter"
             descreption="API-based app that converts currency in real time." 
             github="https://github.com/JAMILA-AIT-HAMOU/Currency-Converter" 
-            demo="https://jamila-ait-hamou.github.io/Currency-Converter/"/>
+            demo="https://jamila-ait-hamou.github.io/Currency-Converter/"
+            icon1={HTMLIcon}
+            icon2={CSSIcon}
+            icon3={JavaScriptIcon}
+            icon4={ReactIcon}/>
       
       <ProjectCard
             imgSrc={screenShot3}
             title="Event RSVP Form"
             descreption="Interactive form built with JavaScript." 
             github="https://github.com/JAMILA-AIT-HAMOU/Event-RSVP-Form" 
-            demo="https://jamila-ait-hamou.github.io/Event-RSVP-Form/"/>
+            demo="https://jamila-ait-hamou.github.io/Event-RSVP-Form/"
+            icon1={HTMLIcon}
+            icon2={CSSIcon}
+            icon3={JavaScriptIcon}
+            icon4={ReactIcon}/>
+
+      <ProjectCard
+            imgSrc={screenShot6}
+            title="EcoBrew Product Landing Page"
+            descreption="A modern product landing page built using HTML and CSS." 
+            github="https://github.com/JAMILA-AIT-HAMOU/EcoBrew-landing-page" 
+            demo="https://jamila-ait-hamou.github.io/EcoBrew-landing-page/"
+            icon1={HTMLIcon}
+            icon2={CSSIcon}
+            />
+
+      <ProjectCard
+            imgSrc={screenShot4}
+            title="Tic Tac Toe Game"
+            descreption="Interactive game built with React." 
+            github="https://github.com/JAMILA-AIT-HAMOU/Tic-Tac-Toe-Game" 
+            demo="https://jamila-ait-hamou.github.io/Tic-Tac-Toe-Game/"
+            icon1={HTMLIcon}
+            icon2={CSSIcon}
+            icon3={JavaScriptIcon}
+            icon4={ReactIcon}/>
       </div>
     </section>
   )
