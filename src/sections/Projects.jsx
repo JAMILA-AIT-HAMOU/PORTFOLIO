f3cc021048b7657dd1f36c6bc2c8c1b5e052e93a
jamila ait <hamouni5099@gmail.com>
2026-03-08 09:24:05 +0000

    edit the projects description

--- src/sections/Projects.jsx
+++ src/sections/Projects.jsx
@@ -20,7 +20,7 @@ function Projects(){
       <ProjectCard
             imgSrc={screenShot1}
             title="Memory Card Game"
-            descreption="React memory game with timer, sound effects, and win/lose logic."
+            descreption="A React memory game where players flip cards to find matching pairs while managing game state and dynamic UI updates."
             github="https://github.com/JAMILA-AIT-HAMOU/Memory-Card-Game" 
             demo="https://jamila-ait-hamou.github.io/Memory-Card-Game/"
             icon1={HTMLIcon}
@@ -31,7 +31,7 @@ function Projects(){
       <ProjectCard
             imgSrc={screenShot5}
             title="Dental Prosthetist Website"
-            descreption="A fully responsive portfolio website built from scratch using HTML and CSS." 
+            descreption="A responsive portfolio websie designed for a dental protsthetist to showcase servises and professional work." 
             github="https://github.com/JAMILA-AIT-HAMOU/PORTFOLIO-PROTHESISTE-DENTAIRE" 
             demo="https://jamila-ait-hamou.github.io/PORTFOLIO-PROTHESISTE-DENTAIRE/#"
             icon1={HTMLIcon}
@@ -43,7 +43,7 @@ function Projects(){
       <ProjectCard
             imgSrc={screenShot2}
             title="Currency Converter"
-            descreption="API-based app that converts currency in real time." 
+            descreption="A React application that converts currencies in real time using an external exchange rate API" 
             github="https://github.com/JAMILA-AIT-HAMOU/Currency-Converter" 
             demo="https://jamila-ait-hamou.github.io/Currency-Converter/"
             icon1={HTMLIcon}
@@ -54,7 +54,7 @@ function Projects(){
       <ProjectCard
             imgSrc={screenShot3}
             title="Event RSVP Form"
-            descreption="Interactive form built with JavaScript." 
+            descreption="A responsive event registration from that handles user input and form validation with a clean UI." 
             github="https://github.com/JAMILA-AIT-HAMOU/Event-RSVP-Form" 
             demo="https://jamila-ait-hamou.github.io/Event-RSVP-Form/"
             icon1={HTMLIcon}
@@ -65,7 +65,7 @@ function Projects(){
       <ProjectCard
             imgSrc={screenShot6}
             title="EcoBrew Product Landing Page"
-            descreption="A modern product landing page built using HTML and CSS." 
+            descreption="A modern product landing page built using HTML and CSS using responsive layout techniques and CSS Grid." 
             github="https://github.com/JAMILA-AIT-HAMOU/EcoBrew-landing-page" 
             demo="https://jamila-ait-hamou.github.io/EcoBrew-landing-page/"
             icon1={HTMLIcon}
@@ -75,7 +75,7 @@ function Projects(){
       <ProjectCard
             imgSrc={screenShot4}
             title="Tic Tac Toe Game"
-            descreption="Interactive game built with React." 
+            descreption="A classic Tic Tac Toe game built with JavaScript featuring turn-based gameplay and win detection logic." 
             github="https://github.com/JAMILA-AIT-HAMOU/Tic-Tac-Toe-Game" 
             demo="https://jamila-ait-hamou.github.io/Tic-Tac-Toe-Game/"
             icon1={HTMLIcon}
