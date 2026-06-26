ea4b9929af709b30bc96524a37c78bf4d3854d84
Jamila Ait Hamou <hamouni5099@gmail.com>
2026-06-26 17:51:45 +0100

    add the hiring card

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,9 +1,41 @@
 import logo from "../assets/logo.jpg"
+import { FaHandSparkles } from "react-icons/fa";
+import { FaCheckCircle } from "react-icons/fa";
 
 function Navbar() {
   return (
     <nav>
-      <a href="#"><img src={logo} alt="Jamila's logo" className="logo"/></a>
+      <div className="logo-container">
+        
+        <a href="#">
+          <img src={logo} alt="Jamila's logo" className="logo"/>
+        </a>
+        <div className="connector"></div>
+        <div className="hiring-card">
+          <div className="avatar">
+            <FaHandSparkles />
+          </div>
+          <h4>Hello!</h4>
+          <p>Available for <br /> <span>new projects</span></p>
+          <div className="feature">
+            <FaCheckCircle className="check-icon"/>
+            Responsive Websites
+          </div>
+          <div className="feature">
+            <FaCheckCircle className="check-icon"/>
+            React Applications
+          </div>
+          <a 
+            href="https://www.fiverr.com/users/aithamoujamila/seller_dashboardP"
+            target="_blank"
+            rel="noopener noreferrer"
+            className="hiring-btn"
+          >
+            Hire Me
+          </a>
+
+        </div>
+      </div>
       <ul>
         <li onClick={()=>document.getElementById("skills").scrollIntoView({behavior: "smooth"})} >Skills</li>
         <li onClick={()=>document.getElementById("projects").scrollIntoView({behavior: "smooth"})} >Projects</li>
